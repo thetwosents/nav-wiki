00e8b0a1de4279f143146515d98563f7cdb431f6
jamiegray <jamiegray2234@gmail.com>
2018-06-24 13:47:26 -0500

    hm not sure what i just did lol

--- wiki.js
+++ wiki.js
@@ -39,12 +39,11 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 
 // copied from https://www.w3schools.com/howto/howto_js_collapsible.asp
 
-// need to make .toclevel-n collapsible 
-// -- by adding the "collapsible" class to it 
+// • need to make .toclevel-n collapsible 
+//   • by adding the "collapsible" class to it 
 
-$("toc").click(function(){
-  $("li").addClass("collapsible");
-});
+
+$("div#toc.toc ul").addClass("collapsible");
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
